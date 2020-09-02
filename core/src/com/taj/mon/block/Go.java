c9e42f5709fca1667260113af840a7fb44eda41b
Tony (Juntao) Hu <807769469hjt@gmail.com>
2020-09-02 13:32:04 -0400

    Make class constants private

--- core/src/com/taj/mon/block/Go.java
+++ core/src/com/taj/mon/block/Go.java
@@ -5,9 +5,9 @@ import com.taj.mon.GameInstance.Task;
 
 public class Go extends SqrBlock {
 
-    public static final int SALARY_MIN = 200;
-    public static final int STEP = 50;
-    public static final int SALARY_MAX = 600;
+    private static final int SALARY_MIN = 200;
+    private static final int STEP = 50;
+    private static final int SALARY_MAX = 600;
 
     public static int salary(int roundCount) {
         return Math.min(SALARY_MIN + (roundCount - 1) * STEP, SALARY_MAX);
